5f1d56570d99a7d5326adf605019e7c07973c613
LostFrequenci <dominik.odron@gmail.com>
2018-11-07 11:54:08 +0100

    Testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,40 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.git ");
+        System.out.println("");
+
+        // Username test
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test is OK ! ");
+        } else {
+            System.out.println("Test ERROR !!!");
+        }
+
+        //Calculator test
+        Calculator calculator = new Calculator();
+
+        int expectedAddingResult = 10;
+        int expectedSubbResult = 4;
+
+        if (calculator.add(7,3) == expectedAddingResult) {
+            System.out.println("Calculator adding test is OK ! ");
+        } else {
+            System.out.println("Calculator adding test ERROR !!!");
+        }
+
+        if (calculator.subtract(7,3) == expectedSubbResult) {
+            System.out.println("Calculator subtraction test is OK ! ");
+        } else {
+            System.out.println("Calculator subtraction test ERROR !!!");
+        }
+
 
     }
 
