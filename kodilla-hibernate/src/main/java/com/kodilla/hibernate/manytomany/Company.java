77978f8191f26978ce939e88bffe57c336aeea9e
LostFrequenci <dominik.odron@gmail.com>
2019-01-09 19:16:53 +0100

    Moduł 13.4

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -5,6 +5,19 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+
+        name = "Company.retrieveWithThreeFirstLetters",
+        query = "SELECT * FROM COMPANIES" +
+                " WHERE SUBSTRING(COMPANY_NAME, 1, 3) = :NAME",
+        resultClass = Company.class
+
+        //name = "Company.retrieveCompaniesWithNamePrefix",
+        //query = "SELECT * FROM COMPANIES" +
+        //        " WHERE COMPANY_NAME LIKE CONCAT(:PREFIX, '%')",
+        //resultClass = Company.class
+)
+
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
